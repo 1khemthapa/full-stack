c8e436194d07b1c76cbd7bba74de367d3b76a6a8
khem <khemthapa231@gmail.com>
2025-07-25 23:01:34 +0545

    banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -10,17 +10,17 @@ const Banner = () => {
   return (
   
         <div>
-        <div className="bg-white items-center text-center   p-6 md:p-8 my-10 max-w-6xl mx-auto flex flex-col gap-6 relative">
-          <img src={assets.docbanner} className="bg-white items-center text-center border border-gray-200 shadow-lg rounded-2xl  my-10 max-w-6xl mx-auto flex flex-col gap-6 object-cover" alt="" />
+        <div className="bg-[#7494EC] items-center text-center   p-6 md:p-8 my-10 max-w-6xl mx-auto flex flex-col gap-6 relative h-60 w-[61rem] rounded-2xl">
+          
       <div className="w-full md:w-[40rem] flex flex-col items-center absolute top-0  h-full justify-center">
-        <h2 className="text-2xl  md:text-3xl font-bold text-gray-800 mb-2">
+        <h2 className="text-2xl  md:text-3xl font-bold text-white mb-2">
           Your Health Deserves Trusted Experts
         </h2>
-        <p className="text-gray-600 text-base md:text-lg mb-4">
+        <p className="text-white text-base md:text-lg mb-4">
           Join thousands of patients who rely on DocTime for easy booking, verified doctors, and personal health tracking. It’s fast, secure, and free to get started.
         </p>
         { token ?
-        <button onClick={()=>navigate('/doctors')} className="bg-primary text-white font-semibold px-6 py-3 rounded-lg shadow-md hover:bg-blue-600 transition">
+        <button onClick={()=>navigate('/doctors')} className="bg-white hover:text-white font-semibold px-6 py-3 rounded-full shadow-md hover:bg-blue-600 transition">
             Get Appointment
           </button>:
 
